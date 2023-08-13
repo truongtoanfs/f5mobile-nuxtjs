f5ba6ad1f1abd8d612e65f879c79dc96752475dd
vToan <toantv@bizflycloud.vn>
2023-08-13 22:24:31 +0700

    feat: create endpoint product

--- f5mobile-server/src/app/controller/productsController.js
+++ f5mobile-server/src/app/controller/productsController.js
@@ -1,11 +1,20 @@
 const pool = require('../../config/db');
-
+const fullProductInfo = `
+    SELECT product.id, name, new_price, old_price, total, sold, description, category, category.link AS category_link, series, series.link AS series_link, color, color.value AS color_value, storage, storage.value AS storage_value, status, status.value AS status_value
+    FROM product LEFT JOIN category ON product.category_id = category.id
+    LEFT JOIN series ON product.series_id = series.id
+    LEFT JOIN color ON product.color_id = color.id
+    LEFT JOIN storage ON product.storage_id = storage.id
+    LEFT JOIN status ON product.status_id = status.id;
+`;
 class ProductsController {
-    index(req, res, next) {
-        pool.query('SELECT * FROM person', (error, results) => {
-            if (error) throw error;
+    async index(req, res, next) {
+        try {
+            const results = await pool.query(fullProductInfo);
             res.status(200).json(results.rows);
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
